3f0fc2f8069a0be9589ac1745dd19d6789e71800
Sheldon <sheldon.hua@gmail.com>
2016-11-27 15:31:03 -0500

    Add Google Image Search api + refactor ajax to use promise

--- app/js/script.js
+++ app/js/script.js
@@ -3,8 +3,11 @@ var app = {};
 app.key = 'a6bb05d3c2ee035be37486ffa23ee061';
 app.id = '49ee0fd8';
 
+app.googleKey = "AIzaSyA6zMuLyh7L4KPbAVuzlNNrzo5WThY4NVg";
+app.searchEngineId = "013423621592567787184:r8jnaxylyeu"
+
 app.ajaxCall = function(searchTerm){
-	$.ajax({
+	var nutritionApi = $.ajax({
 		url: `https://api.nutritionix.com/v1_1/search/${searchTerm}?results=0:10&fields=brand_name,nf_calories,item_name,brand_id`,
 		type: 'GET',
 		dataType: 'json',
@@ -12,21 +15,29 @@ app.ajaxCall = function(searchTerm){
 			appKey: app.key,
 			appId: app.id,
 			format: 'json',
-		},
-		success: function(data){
-
-			var items = data.hits;
-			console.log(items);
-
-			for(var i=0; i<= items.length-1; i++){
-				console.log(items[i].fields.item_name);
-			}
 		}
-	});			
+		
+	});	
+	var googleSearch = $.ajax({
+		url: `https://www.googleapis.com/customsearch/v1?parameters`,
+		type: 'GET',
+		dataType: 'json',
+		data:{
+			key: app.googleKey,
+			cx: app.searchEngineId,
+			q: searchTerm,
+			searchType: "image",
+			format: 'json',
+		},
+	});	
+
+	$.when(nutritionApi, googleSearch).then(function(nutritionApiData, googleSearchData){
+		console.log(nutritionApiData,googleSearchData );
+	})		
 }
 
 app.init = function(){
-	app.ajaxCall("popeyes");
+	app.ajaxCall("burger");
 }
 
 $(function(){
